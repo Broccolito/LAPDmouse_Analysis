b8a7b6bdf76acf9b77febf8ec94458d707a3e95e
Broccolito <wanjungu@yahoo.com>
2019-04-12 20:51:36 -0700

    auto commit

---
+++ airway_tree/airway_tree_with_deposition.R
@@ -0,0 +1,99 @@
+if(!require("rgl")){
+  install.packages("rgl")
+  library("rgl")
+}
+
+get_directory = function(){
+  args <- commandArgs(trailingOnly = FALSE)
+  file <- "--file="
+  rstudio <- "RStudio"
+  match <- grep(rstudio, args)
+  if(length(match) > 0){
+    return(dirname(rstudioapi::getSourceEditorContext()$path))
+  }else{
+    match <- grep(file, args)
+    if (length(match) > 0) {
+      return(dirname(normalizePath(sub(file, "", args[match]))))
+    }else{
+      return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+    }
+  }
+}
+
+wd = get_directory()
+setwd(wd)
+
+#Read the data
+airway_data = read.csv(file = "m01_AirwayTreeTable.csv")
+lobe_depo = read.csv("m01_LobesDeposition.csv")
+sublobe_depo = read.csv("m01_SublobesDeposition.csv")
+acini_depo = read.csv("m01_NearAciniDeposition.csv")
+
+data_dimension = dim(airway_data)[1]
+
+try({
+  rgl.close()
+}, silent = TRUE)
+
+open3d()
+for(i in 1:data_dimension){
+  
+  data = airway_data[i,]
+  airway = cylinder3d(
+    center = cbind(c(data$centroidX + 0.5 * data$length * data$directionX,
+                     data$centroidX - 0.5 * data$length * data$directionX),
+                   c(data$centroidY + 0.5 * data$length * data$directionY,
+                     data$centroidY - 0.5 * data$length * data$directionY),
+                   c(data$centroidZ + 0.5 * data$length * data$directionZ,
+                     data$centroidZ - 0.5 * data$length * data$directionZ)),
+    radius = data$radius
+    
+  )
+  #shade3d(addNormals(subdivision3d(airway, depth = 0)), col = rainbow(data_dimension)[i])
+  shade3d(addNormals(subdivision3d(airway, depth = 0)), col = "yellow")
+  
+}
+
+############### Deposition 
+
+# for(j in 1:dim(lobe_depo)[1]){
+# 
+#   data = acini_depo[j,]
+#   rgl.spheres(x = data$centroidX,
+#               y = data$centroidY,
+#               z = data$centroidZ,
+#               r = data$volume * data$mean / 2000,
+#               color = "green")
+# 
+# }
+
+for(j in 1:dim(sublobe_depo)[1]){
+
+  data = acini_depo[j,]
+  rgl.spheres(x = data$centroidX,
+              y = data$centroidY,
+              z = data$centroidZ,
+              r = data$volume * data$mean / 2000,
+              color = "blue")
+
+}
+
+# for(j in 1:dim(acini_depo)[1]){
+#   
+#   data = acini_depo[j,]
+#   rgl.spheres(x = data$centroidX,
+#               y = data$centroidY,
+#               z = data$centroidZ,
+#               r = data$volume * data$mean / 2000,
+#               color = "grey")
+#   
+# }
+
+
+
+
+
+
+
+
+
