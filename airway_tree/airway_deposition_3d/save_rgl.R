e909aa14e4e348ac2298ff8a30f4dcbdb578b8e9
Broccolito <wanjungu@yahoo.com>
2019-04-14 16:48:50 -0700

    auto commit

--- airway_tree/airway_deposition_3d/save_rgl.R
+++ airway_tree/airway_deposition_3d/save_rgl.R
@@ -1,12 +1,37 @@
-library(rgl)
+if(!require("rgl")){
+  install.packages("rgl")
+  library("rgl")
+}
+
+get_directory = function(){
+  args <- commandArgs(trailingOnly = FALSE)
+  file <- "--file="
+  rstudio <- "RStudio"
+  match <- grep(rstudio, args)
+  if(length(match) > 0){
+    return(dirname(rstudioapi::getSourceEditorContext()$path))
+  }else{
+    match <- grep(file, args)
+    if (length(match) > 0) {
+      return(dirname(normalizePath(sub(file, "", args[match]))))
+    }else{
+      return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+    }
+  }
+}
+
+wd = get_directory()
+setwd(wd)
+
+# file_pat = c("m01")
 
 file_pat = c("m01", "m02", "m03", "m04", "m05", "m06", "m07", "m08", "m09",
              "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18")
 
-plot_inclusive_depo = function(airway_file,
-                               lobe_file,
-                               sublob_file,
-                               acini_file){
+plot_depo = function(airway_file,
+                     lobe_file,
+                     sublob_file,
+                     acini_file){
   
   #Read the data
   airway_data = read.csv(file = airway_file)
@@ -16,6 +41,11 @@ plot_inclusive_depo = function(airway_file,
   
   data_dimension = dim(airway_data)[1]
   
+  try({
+    rgl.close()
+  }, silent = TRUE)
+  
+  open3d()
   for(i in 1:data_dimension){
     
     data = airway_data[i,]
@@ -30,63 +60,70 @@ plot_inclusive_depo = function(airway_file,
       
     )
     #shade3d(addNormals(subdivision3d(airway, depth = 0)), col = rainbow(data_dimension)[i])
-    shade3d(addNormals(subdivision3d(airway, depth = 0)), col = "yellow")
+    shade3d(addNormals(subdivision3d(airway, depth = 0)), col = "grey")
+    
   }
   
   ############### Deposition 
   
+  color_gradient = heat.colors(ceiling(max(lobe_depo$volume * lobe_depo$mean)))
   for(j in 1:dim(lobe_depo)[1]){
     
-    data = acini_depo[j,]
+    data = lobe_depo[j,]
     rgl.spheres(x = data$centroidX,
                 y = data$centroidY,
                 z = data$centroidZ,
-                r = data$volume * data$mean / 2000,
-                color = "green")
+                r = 1.5,
+                color = color_gradient[round(lobe_depo$volume[j] * lobe_depo$mean[j])])
     
   }
   
+  color_gradient = heat.colors(ceiling(max(sublobe_depo$volume * sublobe_depo$mean)))
   for(j in 1:dim(sublobe_depo)[1]){
     
-    data = acini_depo[j,]
+    data = sublobe_depo[j,]
     rgl.spheres(x = data$centroidX,
                 y = data$centroidY,
                 z = data$centroidZ,
-                r = data$volume * data$mean / 2000,
-                color = "blue")
+                r = 0.5,
+                color = color_gradient[round(sublobe_depo$volume[j] * sublobe_depo$mean[j])])
     
   }
   
+  color_gradient = heat.colors(ceiling(max(acini_depo$volume * acini_depo$mean)))
   for(j in 1:dim(acini_depo)[1]){
     
     data = acini_depo[j,]
     rgl.spheres(x = data$centroidX,
                 y = data$centroidY,
                 z = data$centroidZ,
-                r = data$volume * data$mean / 2000,
-                color = "grey")
+                r = 0.15,
+                color = color_gradient[round(acini_depo$volume[j] * acini_depo$mean[j])])
     
   }
-  
   
 }
 
 for(fp in file_pat){
+  
   fl = list.files(pattern = fp)
-  plot_inclusive_depo(airway_file = fl[1], lobe_file = fl[2], sublob_file = fl[4], acini_file = fl[3])
+  plot_depo(airway_file = fl[1], lobe_file = fl[2], sublob_file = fl[4], acini_file = fl[3])
   
   # This writes a copy into temporary directory 'webGL', and then displays it
   filename <- writeWebGL(dir = file.path(tempdir(), paste0(fp, "_plot")), 
                          width = 1000, reuse = TRUE)
+  
   # Display the "reuse" attribute
-  attr(filename, "reuse")
+  # attr(filename, "reuse")
   
   # Display the scene in a browser
-  if (interactive()){
-    browseURL(paste0("file://", filename))
-  }
+  # if (interactive()){
+  #   browseURL(paste0("file://", filename))
+  # }
+  
   try({
     rgl.close()
   }, silent = TRUE)
+  
 }
 
