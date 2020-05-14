c04fd205f1adb51e7a90a0053dcf5180a623c97a
Broccolito <wanjungu@yahoo.com>
2020-05-14 15:28:11 -0700

    auto sync

--- distance_deposition/plotting.R
+++ distance_deposition/plotting.R
@@ -6,6 +6,8 @@ library(ggplot2)
 library(gridExtra)
 library(ggpubr)
 library(ggpmisc)
+library(dplyr)
+library(knitr)
 
 get_directory = function(){
   args <- commandArgs(trailingOnly = FALSE)
@@ -45,16 +47,37 @@ plt1 = ggplot(data = d, aes(x = distance,y = depo)) +
   ggsave(filename = "depo_dist_overall.png",device = "png",
          dpi = 1200,width = 18,height = 9)
 
-d = filter(d,id=="m27"|id=="m08"|id=="m34")
+
+m08_threshold = median(filter(d,id=="m08")$depo)+qnorm(0.99)*sd(filter(d,id=="m08")$depo)
+m27_threshold = median(filter(d,id=="m27")$depo)+qnorm(0.99)*sd(filter(d,id=="m27")$depo)
+m34_threshold = median(filter(d,id=="m34")$depo)+qnorm(0.99)*sd(filter(d,id=="m34")$depo)
+d_m08 = filter(d,id=="m08") %>%
+  mutate(hotspot_color = ifelse(depo>m08_threshold,"black","gray60")) %>%
+  mutate(hotspot_shape = ifelse(depo>m08_threshold,15,16))
+d_m27 = filter(d,id=="m27")%>%
+  mutate(hotspot_color = ifelse(depo>m27_threshold,"black","gray60")) %>%
+  mutate(hotspot_shape = ifelse(depo>m27_threshold,15,16))
+d_m34 = filter(d,id=="m34")%>%
+  mutate(hotspot_color = ifelse(depo>m34_threshold,"black","gray60")) %>%
+  mutate(hotspot_shape = ifelse(depo>m34_threshold,15,16))
+d = rbind.data.frame(d_m08,d_m27,d_m34)
 d$id = factor(d$id,levels = c("m34","m08","m27"))
+
 plt2 = ggplot(data = d, aes(x = distance,y = depo)) + 
-  geom_point(size = 1.8) + 
+  geom_point(size = 1.8, color = d$hotspot_color, shape = d$hotspot_shape) + 
   geom_smooth(method = "lm",color = "black",formula = "y~x") + 
   xlab("Distance (mm)") + 
   ylab("Mean Aerosol Deposition (a.u.)") + 
   facet_wrap(.~id,nrow = 1,scales = "fixed") +
   theme_bw() + 
   theme(text = element_text(size = 20),
-        strip.text = element_blank())
-  ggsave(filename = "fig5.png",device = "png",dpi = 1200,
-         width = 13, height = 6)
+        strip.text = element_blank());plt2
+ggsave(filename = "fig5.png",device = "png",dpi = 1200,
+       width = 13, height = 6)
+
+# 0.5 micron
+summary(lm(depo~distance,data = d_m34))$coefficient
+# 1 micron
+summary(lm(depo~distance,data = d_m08))$coefficient
+# 2 micron
+summary(lm(depo~distance,data = d_m27))$coefficient
